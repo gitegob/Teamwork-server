c20e9b4f302834993d16fc7cf950b41be8156b3c
gitego-brian <gitegob7@gmail.com>
2020-10-05 01:43:57 +0200

    adjustments

--- src/_tests_/articles/create.test.js
+++ src/_tests_/articles/create.test.js
@@ -1,40 +1,37 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
+import { expect } from 'chai';
 import request from 'supertest';
 import app from '../../app';
 import db from '../../config/db';
 import testData from '../_testData_/testData';
 
 describe('Create article tests', () => {
-  beforeAll(async (done) => {
+  before(async () => {
     const res = await request(app).post('/api/auth/signup').send(testData.branSignup);
     testData.branToken = res.body.data.token;
-    done();
   });
-  afterAll(async () => {
+  after(async () => {
     await db.sync({ force: true });
     await db.close();
   });
-  it('POST/ user should create an article', async (done) => {
+  it('POST/ user should create an article', async () => {
     const res = await request(app).post('/api/articles').set('Authorization', `Bearer ${testData.branToken}`).send(testData.articleBody);
-    expect(res.status).toEqual(201);
-    expect(res.body).toHaveProperty('data');
-    expect(res.body.data).toHaveProperty('article');
-    expect(res.body.data).toHaveProperty('id');
-    expect(res.body.data).toHaveProperty('authorId');
+    expect(res.status).to.equal(201);
+    expect(res.body).to.have.property('data');
+    expect(res.body.data).to.have.property('article');
+    expect(res.body.data).to.have.property('id');
+    expect(res.body.data).to.have.property('authorId');
     testData.articleId = res.body.data.id;
-    done();
   });
-  it('POST/ user should not create a article when not logged in', async (done) => {
+  it('POST/ user should not create a article when not logged in', async () => {
     const res = await request(app).post('/api/articles').send(testData.articleBody);
-    expect(res.status).toEqual(401);
-    expect(res.body).toHaveProperty('error');
-    done();
+    expect(res.status).to.equal(401);
+    expect(res.body).to.have.property('error');
   });
-  it('POST/ user should not create an empty article', async (done) => {
+  it('POST/ user should not create an empty article', async () => {
     const res = await request(app).post('/api/articles').set('Authorization', `Bearer ${testData.branToken}`).send({});
-    expect(res.status).toEqual(400);
-    expect(res.body).toHaveProperty('error');
-    done();
+    expect(res.status).to.equal(400);
+    expect(res.body).to.have.property('error');
   });
 });
